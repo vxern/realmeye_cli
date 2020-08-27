ff3beb5c3b04bc1c4f724ff5724a03e812a22013
devongalat <devongalat@gmail.com>
2020-08-27 19:26:49 +0100

    Addition of 'actions' which will be used for offer manipulation.

--- lib/structs.dart
+++ lib/structs.dart
@@ -1,14 +1,55 @@
-class Item {
-  int id;
-  int quantity;
+import 'package:reDart/utils.dart';
 
-  Item(this.id, this.quantity);
+class Item {
+  final int id;
+  final int quantity;
+
+  const Item(this.id, this.quantity);
+
+  @override
+  bool operator ==(Object other) =>
+      other is Item && other.id == id && other.quantity == quantity;
 }
 
 class Offer {
-  List<Item> sellItems;
-  List<Item> buyItems;
-  int volume;
+  final List<Item> sellItems;
+  final List<Item> buyItems;
+  final int volume;
 
-  Offer(this.sellItems, this.buyItems, this.volume);
+  const Offer(this.sellItems, this.buyItems, this.volume);
+
+  @override
+  bool operator ==(Object other) =>
+      other is Offer &&
+      Utils.listEqual(sellItems, other.sellItems) &&
+      Utils.listEqual(buyItems, other.buyItems) &&
+      volume == other.volume;
 }
+
+class ActionSingle {
+  final Instruction instruction;
+  final Item item;
+
+  const ActionSingle(this.instruction, this.item);
+
+  @override
+  bool operator ==(Object other) =>
+      other is ActionSingle &&
+      instruction == other.instruction &&
+      item == other.item;
+}
+
+class ActionFull {
+  final ActionSingle firstAction;
+  final ActionSingle secondAction;
+
+  const ActionFull(this.firstAction, this.secondAction);
+
+  @override
+  bool operator ==(Object other) =>
+      other is ActionFull &&
+      firstAction == other.firstAction &&
+      secondAction == other.secondAction;
+}
+
+enum Instruction { Buy, Sell }
